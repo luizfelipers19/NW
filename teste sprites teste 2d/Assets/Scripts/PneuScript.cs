b656d05e0d4ad86d9f073f12690ffcf3233eee94
Luiz Felipe R <luizfelipers19@gmail.com>
2017-08-08 20:32:02 -0300

    Inclusão de Contador de Pneu, Instruções e Objetivos
    Inclusão dos 3 elementos de Texto + Triggers + Script gameMaster

---
+++ teste sprites teste 2d/Assets/Scripts/PneuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PneuScript : MonoBehaviour
+{
+
+    static bool isColliding;
+
+
+
+    private void Start()
+    {
+        isColliding = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            isColliding = true;
+
+            if (isColliding == true)
+            {
+
+                Destroy(gameObject);
+
+
+            }
+
+        }
+    }
